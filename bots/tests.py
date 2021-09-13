3622093382369c73442b744531a28b1e9da01d8e
Bibekananda Bhuyan <kumarbibek342@gmail.com>
2021-09-13 18:49:25 +0530

    Test

--- bots/tests.py
+++ bots/tests.py
@@ -1,3 +1,5 @@
-from django.test import TestCase
+from django.http import HttpResponse
 
-# Create your tests here.
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
